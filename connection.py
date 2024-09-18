d9a49e1fee51f0255aabd2d753d61279edbf4756
Andre Hirosaki <79878953+capi2@users.noreply.github.com>
2024-09-18 12:26:50 -0400

    Update and rename acessar.py to connection.py

--- connection.py
+++ connection.py
@@ -11,7 +11,8 @@ def connect(config):
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 
-def load_config(filename='database.ini', section='postgresql'):
+
+def load_config(filename, section):
     parser = ConfigParser()
     parser.read(filename)
 
@@ -23,25 +24,35 @@ def load_config(filename='database.ini', section='postgresql'):
             config[param[0]] = param[1]
     else:
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-    
+        
     return config
 
-config = load_config()
+#connects to postgresql and creates amazondb database
+init_config = load_config("database.ini", "postgresql")
+conn = connect(init_config)
+conn.autocommit = True
+cursor = conn.cursor()
+cursor.execute(f"CREATE DATABASE amazondb")
+conn.close()
 
-print(config)
-
-conn = connect(config)
-
+#connects to postgresql and to amazondb database to create tables, values, etc
+amazondb_config = load_config("amazondb.ini", "amazondb")
+conn = connect(amazondb_config)
 conn.autocommit = True
 cursor = conn.cursor()
 
 cursor.execute(f"CREATE TABLE funcionario(nome VARCHAR(20), idade INT, salario DECIMAL(10,2))")
 
-cursor.execute("INSERT INTO funcionario VALUES ('Paula', 2904.57, 26)")
-cursor.execute("INSERT INTO funcionario VALUES ('Jose', 1893.88, 22)")
+cursor.execute("INSERT INTO funcionario VALUES ('Paula', 26, 2348.94)")
+cursor.execute("INSERT INTO funcionario VALUES ('Jose', 22, 1231.39)")
+cursor.execute("INSERT INTO funcionario VALUES ('Rodrigo', 19, 1943.29)")
+cursor.execute("INSERT INTO funcionario VALUES ('Amanda', 26, 3119.22)")
+cursor.execute("INSERT INTO funcionario VALUES ('Joao', 43, 9983.64)")
+cursor.execute("INSERT INTO funcionario VALUES ('Carolina', 31, 7855.33)")
 
 cursor.execute(f"SELECT * FROM funcionario")
 
 rows = cursor.fetchall()
-
 print(rows)
+
+conn.close()
